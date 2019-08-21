c3e4c3d47255fba2b8428fe0c7aba39f45e46c5b
carolinamendezv <nina.mendezv@gmail.com>
2019-08-21 09:48:59 -0400

    hola

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-/*Acá va tu código*/
+const next = document.getElementById('next');
+const firstScreen = document.getElementById('login');
+const options = document.getElementById('options');
+options.style.display='none';
+next.addEventListener('click', function(){
+  const userName = document.getElementById('user').value;
+  document.getElementById('welcome-text').innerHTML ="Bienvenid@ " + userName;
+  firstScreen.style.display = 'none';
+  options.style.display = 'block';
+})
+
+
+const peliculas = document.getElementById('peliculas');
+peliculas.addEventListener('click', () =>{
+  options.style.display = 'none';
+  peliculasQuestion1.style.display = 'block';
+})
